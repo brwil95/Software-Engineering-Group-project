a2aa9104ce4d1e75ad517b6da59aad5a41e30d60
deandraharvin <deandraharvin3@gmail.com>
2019-10-21 14:18:51 -0400

    Created a basic hello world web application to start us off, installed flask

---
+++ app.py
@@ -0,0 +1,16 @@
+import flask, random, os
+
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+def index():
+    r = random.randint(1, 100)
+    return flask.render_template("index.html", random_num=r)
+
+
+app.run(
+    port=int(os.getenv('PORT', 8080)),
+    host=os.getenv('IP', '0.0.0.0'),
+    debug=True
+)
