86e7e6c1668cb65f1ee98eee4f982cb601cc6588
deandraharvin <deandraharvin3@gmail.com>
2019-10-28 15:02:21 -0400

    Added better images manually, API will not allow too many api calls, up to 5

--- app.py
+++ app.py
@@ -17,11 +17,22 @@ Bootstrap(app)
 
 @app.route('/')
 def index():
-    response = apiCalls.api()
-    images = []
-    for i in range(1, 7):
-        images.append(response['hits'][i]['recipe']['image'])
-    return render_template('index.html', image1= images[0], image2= images[1], image3 = images[2], image4= images[3], image5=images[4], image6= images[5])
+    # todo figure out how to do one api call for these images
+    # breakfast = apiCalls.api()
+    # lunch = apiCalls.lunch()
+    # dinner = apiCalls.dinner()
+    # images = []
+    # for i in range(1, 3):
+    #     images.append(breakfast['hits'][i]['recipe']['image'])
+    #     images.append(lunch['hits'][i]['recipe']['image'])
+    #     images.append(dinner['hits'][i]['recipe']['image'])
+    # Automatically set these images from the api, we cannot do too many api calls at once so I had to manually enter these images)
+    return render_template('index.html', image1= "https://www.edamam.com/web-img/e26/e267487d5cd69a038c3284931b4a3c1b.jpg",
+                           image2= "https://www.edamam.com/web-img/ea3/ea3d4f71561834c9ba83a4f94855c1a0.jpg",
+                           image3 = "https://www.edamam.com/web-img/b41/b4132f24268a9faeb8eb84bc164f39c1.jpg",
+                           image4= "https://www.edamam.com/web-img/54d/54d01bbcb5537f018e723982af112c57.jpg",
+                           image5="https://www.edamam.com/web-img/81f/81f405106c1d3a69232c98fba2f05565.jpg",
+                           image6= "https://www.edamam.com/web-img/fb0/fb08a81382ac836ec709fee50d0f5123.jpeg")
 
 
 @app.route('/recipes') #   , methods=['POST'])    -- **Waiting for questionnaire response**
