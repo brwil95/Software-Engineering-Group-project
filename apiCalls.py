80da7e77e5b01d2a32586433849a77baea79c15f
deandraharvin <deandraharvin3@gmail.com>
2019-10-30 11:39:14 -0400

    Changed the names of the question values given to the api, Also using response from quiz.html in the response to query the API

--- apiCalls.py
+++ apiCalls.py
@@ -3,8 +3,8 @@ import json
 
 
 # todo will eventually take in data
-def api():
-    url = 'https://api.edamam.com/search?q=breakfast&app_id=5ef19f8a&app_key' \
+def api(q, health_type, health, diet, calories):
+    url = 'https://api.edamam.com/search?q=' + q +'&health_type=' + health_type +'&health=' + health + '&diet=' + diet +' &calories='+ calories + '&app_id=5ef19f8a&app_key' \
           '=4a892acba31294ff42ec114868b7ffce '
     response = requests.get(url)
     return response.json()
--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 import os, requests, apiCalls
 
-
-
 app = Flask(__name__)
 Bootstrap(app)
 
@@ -21,24 +19,25 @@ def index():
     #     images.append(lunch['hits'][i]['recipe']['image'])
     #     images.append(dinner['hits'][i]['recipe']['image'])
     # Automatically set these images from the api, we cannot do too many api calls at once so I had to manually enter these images)
-    return render_template('index.html', image1= "https://www.edamam.com/web-img/e26/e267487d5cd69a038c3284931b4a3c1b.jpg",
-                           image2= "https://www.edamam.com/web-img/ea3/ea3d4f71561834c9ba83a4f94855c1a0.jpg",
-                           image3 = "https://www.edamam.com/web-img/b41/b4132f24268a9faeb8eb84bc164f39c1.jpg",
-                           image4= "https://www.edamam.com/web-img/54d/54d01bbcb5537f018e723982af112c57.jpg",
+    return render_template('index.html',
+                           image1="https://www.edamam.com/web-img/e26/e267487d5cd69a038c3284931b4a3c1b.jpg",
+                           image2="https://www.edamam.com/web-img/ea3/ea3d4f71561834c9ba83a4f94855c1a0.jpg",
+                           image3="https://www.edamam.com/web-img/b41/b4132f24268a9faeb8eb84bc164f39c1.jpg",
+                           image4="https://www.edamam.com/web-img/54d/54d01bbcb5537f018e723982af112c57.jpg",
                            image5="https://www.edamam.com/web-img/81f/81f405106c1d3a69232c98fba2f05565.jpg",
-                           image6= "https://www.edamam.com/web-img/fb0/fb08a81382ac836ec709fee50d0f5123.jpeg")
+                           image6="https://www.edamam.com/web-img/fb0/fb08a81382ac836ec709fee50d0f5123.jpeg")
 
 
-
-@app.route('/recipes', methods=['POST'])  # , methods=['POST'])    -- **Waiting for questionnaire response**
+@app.route('/recipes', methods=['POST'])
 # The API_Calls are shooting me with an error. Please advise on the modules that need to be installed to make it work properly.
 def recipes():
-    response = apiCalls.api()
-    print(flask.request.form)
+    user_response = flask.request.form
+    response = apiCalls.api(user_response['food_type'], user_response['health_type'], user_response['health'], user_response['diet'], user_response['calories'])
+    print(user_response['q1answer'])
     return render_template("response.html", response=response['hits'][0]['recipe']['url'])
 
 
-#quiz questions
+# quiz questions
 @app.route('/quiz')
 def questions():
     q1 = "Are you on a diet? If so which one"
@@ -56,14 +55,12 @@ def questions():
         question5=q5
     )
 
-#stores users answers
+
+# stores users answers
 @app.route('/answers', methods=['POST'])
 def user_answers():
     value = requests.questions['value']
     return value
-
-
-
 
 
 app.run(
