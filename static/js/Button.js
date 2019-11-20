f5e7772a486854868da6f0b3f7616a5dca3ff04f
brianawilliams <williamsb16@comcast.net>
2019-11-19 19:31:25 -0500

    Added the login button

---
+++ static/js/Button.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { Socket } from './Socket';
+import { GoogleLogin } from 'react-google-login';
+
+
+export class Button extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event){
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+
+        let auth = gapi.auth2.getAuthInstance();
+        let user = auth.currentUser.get();
+        if(user.isSignedIn()){
+            console.log("google token" + user.getAuthResponse().id_token);
+        }
+    }
+
+
+
+
+
+
+
+}
